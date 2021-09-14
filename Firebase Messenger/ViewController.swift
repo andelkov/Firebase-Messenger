efeec31de083950f9e22f9152d68edacb5407e49
andelkov <andelkov@gmail.com>
2021-09-14 12:09:18 +0200

    Initial Commit

---
+++ Firebase Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase Messenger
+//
+//  Created by Anđelko on 14.Sep.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
